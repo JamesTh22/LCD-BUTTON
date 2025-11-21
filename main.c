833d95797b01d0045dbc1e4f46db61904c20cc3b
JamesTh22 <229487037+JamesTh22@users.noreply.github.com>
2025-11-21 13:55:31 +0000

    feat: added the main file

---
+++ main.c
@@ -0,0 +1,34 @@
+#include <xc.h>
+#include "lcd_opcodes.h"
+#include "lcd_pinmap.h"
+#include "button_opcodes.h"
+#include "button_pinmap.h"
+
+#define _XTAL_FREQ 20000000UL
+
+#pragma config FOSC = HSHP
+#pragma config PLLCFG = OFF
+#pragma config PRICLKEN = ON
+#pragma config WDTEN = OFF
+#pragma config MCLRE = EXTMCLR
+#pragma config LVP = OFF
+
+void main(void) {
+    lcd_init();
+    buttons_init();
+
+    lcd_puts_at(0, 0, "Ready");
+
+    while (1) {
+        if (isPressedFalling_RE2()) { // OK button
+            lcd_puts_at(0, 0, "hello world");
+            lcd_puts_at(0, 1, "06:45 ALM 07:00");
+        }
+        if (isPressedFalling_RE0()) { // UP button
+            lcd_puts_at(0, 0, "pizza");
+        }
+        if (isPressedFalling_RE3()) { // DOWN button
+            lcd_puts_at(0, 0, "burger");
+        }
+    }
+}
